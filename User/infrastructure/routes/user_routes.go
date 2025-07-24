890d93071ed08c9c873010d15d69b8adb37fdbc9
Cato <catohjmz08@gmail.com>
2025-07-24 01:18:34 -0600

    Fix/hello

--- User/infrastructure/routes/user_routes.go
+++ User/infrastructure/routes/user_routes.go
@@ -1,29 +1,28 @@
 package routes
 
 import (
-    "github.com/bchanona/profile_warmheart_backend/User/infrastructure/controllers"
-    "github.com/bchanona/profile_warmheart_backend/middlewares"
-    "github.com/gin-gonic/gin"
+	"github.com/bchanona/profile_warmheart_backend/User/infrastructure/controllers"
+	"github.com/bchanona/profile_warmheart_backend/middlewares"
+	"github.com/gin-gonic/gin"
 )
 
-func SetupUserRoutes(router *gin.Engine, createUserCtrl *controllers.SaveUserController, 
-    loginUserCtrl *controllers.LoginUserController, getAllUsersCtrl *controllers.GetAllUsersController,
-    getByIDUserCtrl *controllers.GetByIDUserController, updateStatusCtrl *controllers.UpdateStatusController,
-    deleteUserCtrl *controllers.DeleteUserController,saveNotification *controllers.SaveNotificationController,
-    getNotification *controllers.GetNotificationController, jwtKey []byte) {
+func SetupUserRoutes(router *gin.Engine, createUserCtrl *controllers.SaveUserController,
+	loginUserCtrl *controllers.LoginUserController, getAllUsersCtrl *controllers.GetAllUsersController,
+	getByIDUserCtrl *controllers.GetByIDUserController, updateStatusCtrl *controllers.UpdateStatusController,
+	deleteUserCtrl *controllers.DeleteUserController, saveNotification *controllers.SaveNotificationController,
+	getNotification *controllers.GetNotificationController, jwtKey []byte) {
 
-    userGroup := router.Group("/user")
-    {
-        userGroup.POST("/", createUserCtrl.Save)
-        userGroup.POST("/login", loginUserCtrl.Login)
-        userGroup.POST("/saveNotification",saveNotification.Execute)
-        
-        userGroup.Use(middlewares.AuthMiddleware())
-        {
-            userGroup.GET("/", getAllUsersCtrl.GetAll)
-            userGroup.PUT("/updateStatus", updateStatusCtrl.UpdateStatus)
-            userGroup.DELETE("/:id", deleteUserCtrl.Delete)
-            userGroup.GET("/getNotifications",getNotification.Execute)
-        }
-    }
+	userGroup := router.Group("/user")
+	{
+		userGroup.POST("/", createUserCtrl.Save)
+		userGroup.POST("/login", loginUserCtrl.Login)
+		userGroup.POST("/saveNotification", saveNotification.Execute)
+		userGroup.GET("/getNotifications", middlewares.AuthSupervisorMiddleware(), getNotification.Execute) //hola toy aqui :)
+		userGroup.Use(middlewares.AuthMiddleware())
+		{
+			userGroup.GET("/", getAllUsersCtrl.GetAll)
+			userGroup.PUT("/updateStatus", updateStatusCtrl.UpdateStatus)
+			userGroup.DELETE("/:id", deleteUserCtrl.Delete)
+		}
+	}
 }
